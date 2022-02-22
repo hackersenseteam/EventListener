3eaa22b902d9ccb51df0a78906e2b84568c12d9b
BlueCat-Frisk <100213430+BlueCat-Frisk@users.noreply.github.com>
2022-02-22 23:29:56 +0800

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,12 @@
 import jp.timeline.EventSystem.EventManager;
 import jp.timeline.EventSystem.events.SetupEvent;
+import jp.timeline.EventSystem.type.EventType;
 
 public class Main {
     public static void main(String[] args) {
         EventManager.addListener(new Setup());
-        EventManager.call(new SetupEvent());
+        EventManager.call(new SetupEvent("PRE", EventType.PRE));
+        System.out.println("Call");
+        EventManager.call(new SetupEvent("POST", EventType.POST));
     }
 }
