7cc977bab646f467a94a41e83bc8d45894464f9b
BlueCat-Frisk <100213430+BlueCat-Frisk@users.noreply.github.com>
2022-02-22 23:28:45 +0800

    Update SetupEvent.java

--- src/jp/timeline/EventSystem/events/SetupEvent.java
+++ src/jp/timeline/EventSystem/events/SetupEvent.java
@@ -2,9 +2,19 @@ package jp.timeline.EventSystem.events;
 
 import jp.timeline.EventSystem.EventCore;
 import jp.timeline.EventSystem.Events;
+import jp.timeline.EventSystem.type.EventType;
 
 public class SetupEvent extends EventCore {
-    public SetupEvent() {
+    private final String text;
+    
+    public SetupEvent(String text, EventType type) {
         super(Events.SETUP);
+        this.setType(type);
+        this.text = text;
+    }
+    
+    public String getText()
+    {
+        return this.text;
     }
 }
